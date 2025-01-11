f0c6afe280c61c6fcf69171dc7da5da9df706b73
Yash Wankhade <yashwankhade5@gmail.com>
2025-01-10 23:36:36 -0800

    timer input add

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState,useContext} from 'react'
+
+import  "./Timerstyle.css";
+import { createContext } from 'react';
+const shownum = createContext()
 
 const Timer = () => {
+  const [value, setValue] = useState("Default Value");
   return (
-    <div>Timer</div>
+    <div className='timerApp'>
+      <shownum.Provider value={{value,setValue}}>
+     
+      <InputValue/><span className='colon'>:</span>
+      <InputValue/><span className='colon'>:</span>
+      <InputValue/></shownum.Provider>
+    </div>
   )
 }
+
+function InputValue(){
+ let {value,setValue}=useContext(shownum)
+  const handleChange=(e)=>{
+    setValue(e.target.value)
+  }
+return(
+  <input   onChange={handleChange} value={value}  className={'timeInput timeUnit'} />
+)
+
+}
+
 
 export default Timer
