d180822629bd7a9d2b3c65f4b3cb6b2ebc54848d
Yash Wankhade <yashwankhade5@gmail.com>
2025-01-13 02:02:23 -0800

    almost timer complete

--- week-9/timerApp/src/components/Ti.jsx
+++ week-9/timerApp/src/components/Ti.jsx
@@ -42,19 +42,31 @@ function InputValue(){
 const[hr,sethr]=useState(0)
 const[min,setmin]=useState(0)
 const[sec,setsec]=useState(0)
+const second=useRef()
+const minutes =useRef()
 
 function calculateTimme(hr,min,sec) {
   const totaltime=(parseInt(hr)*3600 || 0)+(parseInt(min)*60 || 0)+parseInt(sec || 0)
   setTime(totaltime)
 }
 
-function handlechange(e,unit) {
+ function handlechange(e,unit) {
   const value= parseInt(e.target.value)
+
   if (unit==="hr") {
+    if (value.toString().length>2) {
+      minutes.current.focus()
+      return
+    }
     
-    sethr(value)
+    sethr(c=>c=value)
+    
     calculateTimme(value,min,sec)
   } else if(unit === "min"){
+    if (value.toString().length>2) {
+      second.current.focus()
+      return
+    }
     setmin(value)
     calculateTimme(hr,value,sec)
   }else if(unit==="sec"){
@@ -68,10 +80,11 @@ useEffect(()=>{
  setsec(Math.floor((time% 60)))
 },[time])
 
+
 return(<>
- <input type="number" value={hr.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'hr')}}className={'timeInput timeUnit'}/><span className='colon'>:</span>
-  <input type="number" value={min.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'min')}} className={'timeInput timeUnit'}/><span className='colon'>:</span>
-  <input type="number" value={sec.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'sec')}} className={'timeInput timeUnit'}/>
+ <input  type="number" value={hr.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'hr')}}className={'timeInput timeUnit'}/><span className='colon'>:</span>
+  <input  ref={minutes} type="number" value={min.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'min')}} className={'timeInput timeUnit'}/><span className='colon'>:</span>
+  <input ref={second}type="number" value={sec.toString().padStart(2,"0")} onChange={(e)=>{handlechange(e,'sec')}} className={'timeInput timeUnit'}/>
   </>
 )
 
