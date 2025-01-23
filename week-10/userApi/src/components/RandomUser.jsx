d80016cc30ebd315044355f847cd855633572425
Yash Wankhade <yashwankhade5@gmail.com>
2025-01-23 03:13:24 -0800

    creating card component

--- week-10/userApi/src/components/RandomUser.jsx
+++ week-10/userApi/src/components/RandomUser.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 const RandomUser = () => {
-  return (
-    <div>RandomUser</div>
+  const [user ,setuser]=useState([])
+  async function userid() {
+    const response = await axios.get("https://randomuser.me/api/")
+    console.log(response.data.results)
+    return response.data
+  }
+  useEffect(()=>{
+    const userinfo=  userid()
+
+  },[])
+
+  return (<div>
+    <div style={{margin:"50px 10px",fontWeight:"bold",fontSize:"35px",textAlign:"center"}}>RandomUser</div>
+    <div style={{display:"flex", gap:"30px"}}>
+<Card/>
+<Card/>
+<Card/>
+    </div>
+    
+    </div>
   )
+}
+
+function Card() {
+  return<div style={{borderRadius:"5px",border:"0.5px solid black",width:"100px"}}>
+<img src="https://randomuser.me/api/portraits/med/women/70.jpg" alt="" />
+<span>Cristina</span>
+  </div>
 }
 
 export default RandomUser
