96803cb1c566af12f122770532280ff42b1e705d
Yash Wankhade <yashwankhade5@gmail.com>
2025-01-27 23:50:47 -0800

    items component ongoing

--- week-11/amazonCart/src/components/AmazonCart.jsx
+++ week-11/amazonCart/src/components/AmazonCart.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
  function AmazonsCart() {
-    return<div className=" flex ">
+    return<div className="  flex ">
         
         <div className=" mt-7 w-300">
-        <div className=" bg-white py-4 px-6">Shopping Cart</div>
-        <div className="bg-white "><Items/>
-        
+        <div className="rounded-t-lg bg-white py-4 px-6">Shopping Cart</div>
+        <div className="bg-white rounded-b-lg"><Items/>
+        <Items/>
+        <Items/>
          </div>
       
     </div>
-        <div className="bg-white mt-7 w-79 h-54 ml-4   ">
+        <div className="rounded-md bg-white mt-7 w-79 h-54 ml-4   ">
         to
     </div>
     </div>
@@ -32,14 +33,15 @@ function sub() {
     
 }
 
-    return<div className="flex px-5 py-2 pb-5">
-      <div className="mr-6"><img className="w-40 h-30 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s" alt="" srcset="" /></div>
+    return<div className="pb-4"><div className="flex px-5  ">
+      <div className="mr-7"><img className="w-25 h-23 border-2 " src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" srcset="" /></div>
       <div className="w-full"><div>The Art of Impossible: A Peak Performance Primer</div>
-      <div className="flex justify-between"><div>In stock</div><div></div>₹369.00</div>
-      <div className="flex relative top-12"><button className="bg-gray-400 w-6 items-center mx-2  text-white text-center flex justify-center" onClick={sub}>-</button><div>{quant}</div><button className="bg-[#f2f2f2] w-6 items-center mx-2  text-white text-center flex justify-center"  onClick={add}>+</button> <div className="text-blue-600 font-[500]">Delete</div></div>
+      <div className="flex justify-between pt-2"><div className="text-green-600  font-medium">In stock</div><div></div>₹369.00</div>
+      <div className="flex relative top-3"><button className="bg-gray-400 w-6 items-center mx-2  text-white text-center flex justify-center" onClick={sub}>-</button><div>{quant}</div><button className="bg-[#f2f2f2] w-6 items-center mx-2  text-white text-center flex justify-center"  onClick={add}>+</button> <div className="text-blue-600 font-[500]">Delete</div></div>
       
       </div>
-    </div>
+      
+    </div><div className="pt-6"><hr className="w-19/20 ml-5   color text-gray-300 " /></div></div>
 }
 
 
